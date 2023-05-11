8ea6703ab8be47a121963174da58cef0411d208e
Aathif_Mahir <aathifmahir@gmail.com>
2023-05-12 01:14:09 +0530

    Added Sample Project

---
+++ sample/MauiShakeDetectorSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiShakeDetectorSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ sample/MauiShakeDetectorSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiShakeDetectorSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

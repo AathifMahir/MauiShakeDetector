c49b1e05fa0c3ca4f6f360108fb02d9c7dfbcc7e
Aathif_Mahir <aathifmahir@gmail.com>
2023-05-12 01:16:31 +0530

    Initial Building Blocks and Improvements

---
+++ src/MauiShakeDetector/ShakeDetector.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiShakeDetector;
+public static class ShakeDetector
+{
+    public static bool IsSupported => Default.IsSupported;
+    public static bool IsMonitoring => Default.IsMonitoring;
+    public static double ShakeThresholdGravity => Default.ShakeThresholdGravity;
+    public static TimeSpan ShakeIntervalInMilliseconds => Default.ShakeIntervalInMilliseconds;
+    public static TimeSpan ShakeCountResetTime => Default.ShakeCountResetTime;
+    public static int MinimumShakeCount => Default.MinimumShakeCount;
+    public static bool IsHapticsSupported => Default.IsHapticsSupported;
+    public static bool IsHapticsEnabled => Default.IsHapticsEnabled;
+    public static TimeSpan HapticsDurationInMilliseconds => Default.HapticsDurationInMilliseconds;
+
+    public static event EventHandler<ShakeDetectedEventArgs> ShakeDetected
+    {
+        add => Default.ShakeDetected += value;
+        remove => Default.ShakeDetected -= value;
+    }
+
+    public static void StartListening() => Default.StartListening();
+
+    public static void StopListening() => Default.StopListening();
+
+#nullable enable
+    internal static void SetDefault(IShakeDetector? implementation) =>
+        defaultShakeDetector = implementation;
+
+    static IShakeDetector? defaultShakeDetector;
+
+    public static IShakeDetector Default =>
+            defaultShakeDetector ??= new ShakeDetectorDefault();
+}
---
+++ src/MauiShakeDetector/ShakeEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiShakeDetector;
+public class ShakeDetectedEventArgs : EventArgs
+{
+    public ShakeDetectedEventArgs(int noOfShake) => NoOfShakes = noOfShake;
+    public int NoOfShakes { get; private set; }
+}
